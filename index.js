8fa18bf1116b70a8523384de1a7bf912a5f6600b
Gianluca Bredice <bredicegian@gmail.com>
2024-08-22 15:10:12 -0300

    feature: add required endpoint for pass required tests

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express")
+var cors = require("cors")
+require("dotenv").config()
+const multer = require("multer")
 
-var app = express();
+var app = express()
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(cors())
+app.use("/public", express.static(process.cwd() + "/public"))
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html")
+})
 
+const upload = multer({ dest: "uploads/" })
 
+app.post("/api/fileanalyse", upload.single("upfile"), function (req, res) {
+  const { file } = req
+  console.log(file)
 
+  return res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size,
+  })
+})
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
-});
+  console.log("Your app is listening on port " + port)
+})
